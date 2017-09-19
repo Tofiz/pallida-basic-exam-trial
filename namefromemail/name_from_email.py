80754e5f0a962f284c68bd9d5b4cfa07ff31429e
Tofiz <tofi.zagon@gmail.com>
2017-09-19 15:09:47 +0200

    feat(name_from_email.py):done

--- namefromemail/name_from_email.py
+++ namefromemail/name_from_email.py
@@ -4,5 +4,12 @@
 # last_name first_name
 # example: "elek.viz@exam.com" for this input the output should be: "Viz Elek"
 # accents does not matter
+email = str(input("email: "))
+def name_from_email(e):
+    cut_from_at = e.partition('@')[0]
+    last_name = str(cut_from_at.split('.')[0])
+    first_name = str(cut_from_at.split('.')[1])
+    name = (first_name.title() + " " + last_name.title())
+    return name
 
-print(name_from_email("elek.viz@exam.com"))
+print(name_from_email(email))
