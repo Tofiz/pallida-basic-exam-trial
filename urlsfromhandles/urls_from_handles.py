9859ceae1e7c9c7b2346a4f9ed72760a3dbe4d18
Tofiz <tofi.zagon@gmail.com>
2017-09-19 17:19:19 +0200

    feat(urls_from_handles.py):done

--- urlsfromhandles/urls_from_handles.py
+++ urlsfromhandles/urls_from_handles.py
@@ -7,5 +7,16 @@
 # input: ["ghhandle1", "ghhandle2"]
 # output: ["https://github.com/greenfox-academy/ghhandle1", "https://github.com/greenfox-academy/ghhandle2"]
 
-names = ["ghhandle1", "ghhandle2"]
+names = ["ghhandle1", "ghhandle2", "Tofiz"]
+
+def urls_from_handles(users):
+    url = []
+    for i in range(len(users)):
+        url.append("https://github.com/greenfox-academy/" + users[i])
+    return url
+
+
+
+
+# urls_from_handles(names)
 print(urls_from_handles(names))
