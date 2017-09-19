b513153da6dab3950228940c5a1750214895a433
Tofiz <tofi.zagon@gmail.com>
2017-09-19 16:25:00 +0200

    feat(dictionary.py):under writing

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -1,4 +1,4 @@
-dictionary = [
+word_dictionary = [
     {"alma": "apple"},
     {"fa": "tree"}
 ]
@@ -7,16 +7,26 @@ dictionary = [
 # list 'dictionary'
 
 
-def add_word(hun_word, eng_word):
-    pass
+def add_word(dictionary):
+    hun_word = input("Type the hungarian word: ")
+    eng_word = input("type it in english: ")
+    dictionary.append({hun_word:eng_word})
+    return dictionary
 
+add_word(word_dictionary)
 # Implement these methods. They should return the translation of the given
 # word form the list 'dictionary'
-
+print(word_dictionary)
 
 def translate_to_hun(eng_word):
     pass
 
 
-def translate_to_eng(hun_word):
-    pass
+def translate_to_eng(d):
+    given_word = input("HUN to ENG dictionary: ")
+    for k in d:
+        if k in d.itervalues():
+            print("ok")
+        else:
+            print(given_word)
+translate_to_eng(word_dictionary)
